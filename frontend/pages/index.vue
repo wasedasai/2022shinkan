9c578900327a86399c2f51fe6e6abdde142c86bb
akino <akikoba2001@gmail.com>
2022-02-26 13:04:39 +0900

    static check1

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <img class="saikun" src="/ase.png" alt="先進理工学部" style="display: block; margin: auto;">
+    <img
+      class="saikun"
+      src="/ase.png"
+      alt="先進理工学部"
+      style="display: block; margin: auto"
+    />
+    <AtomsParagraph>Hello</AtomsParagraph>
   </div>
 </template>
 
