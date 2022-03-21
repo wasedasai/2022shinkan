ac0f9d182c8ef47b2f81adadd7e0265d4de5fa9a
aino <aino.takahashi@wasedasai.net>
2022-03-21 22:22:14 +0900

    headerを修正しました

--- frontend/components/organisms/header/index.vue
+++ frontend/components/organisms/header/index.vue
@@ -3,13 +3,21 @@ export default {
   props: {
     hasHeader: {
       type: Boolean,
-      default: true
+      default: true,
+    },
+    isInternal: {
+      type: Boolean,
+      default: true,
     }
   },
-  data () {
+  data() {
     return {
-      isHeaderOpen: false
+      isHeaderOpen: false,
+      path: ''
     }
+  },
+  mounted () {
+    this.path = this.$route.path
   }
 }
 </script>
@@ -19,27 +27,93 @@ export default {
     class="header"
     :class="{
       hasHeader,
-      }"
+    }"
   >
-  <div
+    <div
       class="pc"
       :class="{
         hasHeader,
       }"
     >
       <h1>
-        <img class="logo_with_catch" src="~assets/images/logo_with_catch.png" alt="headerロゴ">
+        <img
+          class="logo_with_catch"
+          src="~assets/images/logo_with_catch.png"
+          alt="headerロゴ"
+        />
       </h1>
       <nav>
         <ul class="main-nav">
           <li v-show="hasHeader">
-            <NuxtLink :to="'/'">HOME</NuxtLink>
-           </li>
-          <li><NuxtLink :to="'/#about-waedasai-link'">早稲田祭とは</NuxtLink></li>
-          <li><NuxtLink :to="'/#about-unsta-link'">運スタとは</NuxtLink></li>
-          <li><NuxtLink :to="'/#admission-link'">入会方法</NuxtLink></li>
-          <li><NuxtLink :to="'/#event-link'">新歓イベント</NuxtLink></li>
-          <li><NuxtLink :to="'/contact'">お問い合わせ</NuxtLink></li>
+            <NuxtLink
+              v-if="isInternal"
+              :to="'/'"
+              class="current"
+              :class="{
+                underline: path == '/componentslist',
+              }"
+            >
+              HOME
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+            v-if="isInternal"
+            :to="'/about-waedasai-link'"
+             class="current"
+            :class="{
+            underline: path == '/wasedasaitoha',
+            }"
+             >
+             早稲田祭とは
+             </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+             v-if="isInternal"
+             :to="'/about-unsta-link'"
+             class="current"
+            :class="{
+            underline: path == '/unstatoha',
+            }"
+             >
+             運スタとは
+             </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+            v-if="isInternal"
+            :to="'/admission-link'"
+            class="current"
+            :class="{
+            underline: path == '/nyukaihouhou',
+            }"
+            >
+            入会方法
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+            v-if="isInternal"
+            :to="'/event-link'"
+            :class="{
+            underline: path == '/eventichiran',
+            }"
+            >
+            新歓イベント
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+            v-if="isInternal"
+            :to="'/contact'"
+            :class="{
+            underline: path == '/otoiawase',
+            }"
+            >
+            お問い合わせ
+            </NuxtLink>
+          </li>
         </ul>
       </nav>
     </div>
@@ -51,58 +125,74 @@ export default {
       }"
     >
       <h1 v-show="hasHeader">
-        <img class="logo_with_catch_sp" src="~assets/images/logo_with_catch.png" alt="headerロゴ">
+        <img
+          class="logo_with_catch_sp"
+          src="~assets/images/logo_with_catch.png"
+          alt="headerロゴ"
+        />
       </h1>
 
       <div class="open-button_container">
-        <button class="open-button" @click="isHeaderOpen=true">
+        <button class="open-button" @click="isHeaderOpen = true">
           <span />
           <span />
           <span />
         </button>
       </div>
-      <div class="menu-bg" :class="{open:isHeaderOpen}">
+      <div class="menu-bg" :class="{ open: isHeaderOpen }">
         <div class="sp-nav_header">
           <div class="close-button_container">
-            <button class="close-button" @click="isHeaderOpen=false">
+            <button class="close-button" @click="isHeaderOpen = false">
               <span />
               <span />
               <span />
             </button>
           </div>
         </div>
-      <nav class="sp-nav_contents">
+        <nav class="sp-nav_contents">
           <ul class="sp-nav">
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/#about-waedasai-link'">HOME</NuxtLink></li>
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/#about-unsta-link'">早稲田祭とは</NuxtLink></li>
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/#about-unsta-link'">運スタとは</NuxtLink></li>
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/#admission-link'">入会方法</NuxtLink></li>
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/#event-link'">新歓イベント</NuxtLink></li>
-            <li @click="isHeaderOpen=false"><NuxtLink :to="'/contact'">お問い合わせ</NuxtLink></li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/'">HOME</NuxtLink>
+            </li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/about-wasedasai-link'">早稲田祭とは</NuxtLink>
+            </li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/about-unsta-link'">運スタとは</NuxtLink>
+            </li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/admission-link'">入会方法</NuxtLink>
+            </li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/event-link'">新歓イベント</NuxtLink>
+            </li>
+            <li @click="isHeaderOpen = false">
+              <NuxtLink :to="'/contact'">お問い合わせ</NuxtLink>
+            </li>
           </ul>
-      </nav>
+        </nav>
       </div>
     </div>
   </header>
 </template>
 
 <style scoped>
-.header{
+.header {
   position: fixed;
   z-index: 990;
   width: 100%;
-  background:#dde6cb;
+  background: #dde6cb;
 }
 .header.hasHeader {
-  background:#dde6cb;
+  background: #dde6cb;
   cursor: pointer;
 }
-.logo_with_catch{
+.logo_with_catch {
   width: 250px;
   margin-left: 5rem;
 }
 .header .pc {
-  padding: .5rem;
+  padding: 0.5rem;
   max-width: 1440px;
   margin: auto;
 }
@@ -115,7 +205,7 @@ export default {
 .header .sp.hasHeader {
   justify-content: space-between;
 }
-.logo_with_catch_sp{
+.logo_with_catch_sp {
   width: 150px;
   margin-left: 1rem;
   margin-top: -3rem;
@@ -139,9 +229,9 @@ export default {
 .open-button_container .open-button span {
   width: 40px;
   height: 4px;
-  padding:0rem ;
+  padding: 0rem;
   margin-right: 3rem;
-  background-color: #804B40;
+  background-color: #804b40;
 }
 .menu-bg {
   position: fixed;
@@ -192,28 +282,28 @@ export default {
 
 .close-button_container .close-button span:first-child {
   transform: rotate(45deg);
-  background-color: #804B40;
+  background-color: #804b40;
 }
 
 .close-button_container .close-button span:last-child {
   transform: rotate(-45deg);
-  background-color: #804B40;
+  background-color: #804b40;
 }
 .sp-nav li {
   text-align: center;
 }
 
-.sp-nav li a{
-  color: #804B40;
+.sp-nav li a {
+  color: #804b40;
   text-decoration: none;
   font-weight: bold;
   display: block;
   width: 100%;
   height: 100%;
-  padding: .5rem;
+  padding: 0.5rem;
 }
 
-@media screen and (min-width: 1024px){
+@media screen and (min-width: 1024px) {
   .header .sp {
     display: none;
   }
@@ -228,24 +318,40 @@ export default {
     justify-content: space-between;
   }
 
-  .main-nav{
+  .main-nav {
     display: flex;
     font-size: 1.5rem;
     padding-right: 5rem;
+  }
+
+  .underline {
+    text-decoration: underline;
+    position: relative;
+  }
+
+  .underline::after {
+    content: '';
+    display: block;
+    width: 60px;
+    height: 3px;
+    background-color: #804b40;
+    position: absolute;
+    bottom: 0;
+    left: 10px;
   }
 
   .main-nav li a {
     display: block;
     width: 100%;
     padding: 1rem;
-    transition: all .2s;
-    color: #804B40;
+    transition: all 0.2s;
+    color: #804b40;
     text-decoration: none;
     font-weight: bold;
   }
 
   .main-nav li a:hover {
-    color: #fff;
+    color: #4c2219;
   }
 }
 </style>
