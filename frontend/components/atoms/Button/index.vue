a4be729eeec9a4c538793d4004449463afc95b98
aino <aino.takahashi@wasedasai.net>
2022-03-21 13:36:18 +0900

    lint

--- frontend/components/atoms/Button/index.vue
+++ frontend/components/atoms/Button/index.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="button">
-    <h2 class="content">
-      ボタン
-    </h2>
+    <h2 class="content">ボタン</h2>
   </div>
 </template>
 
 <style scoped>
-.button{
+.button {
   width: 200px;
   height: 72px;
   background-color: #fff;
-  border-color: #804B40;
+  border-color: #804b40;
   border-style: solid;
   border-width: 4px;
   border-radius: 30px;
-  padding: .5rem;
+  padding: 0.5rem;
   cursor: pointer;
   transition: all 0.3s;
   box-shadow: 2px 2px 4px gray;
 }
 
-.button:hover{
+.button:hover {
   filter: drop-shadow(8px 8px 4px rgb(0 0 0 / 0.4));
 }
 
-.content{
-  color: #804B40;
+.content {
+  color: #804b40;
   text-decoration: none;
   font-size: 16px;
   text-align: center;
