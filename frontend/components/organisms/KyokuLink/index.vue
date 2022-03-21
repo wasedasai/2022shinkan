ea45b9c6443608788430b5e59abcc7b4ee8f4acf
akino <akikoba2001@gmail.com>
2022-03-21 10:22:26 +0900

    局リンクカード作成

---
+++ frontend/components/organisms/KyokuLink/index.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  props: {
+    url: { type: String, default: '' },
+    bgType: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="kyokulink" :class="bgType">
+    <nuxt-link :to="url" class="link"
+      ><div class="img_filter">
+        <h2><slot /></h2></div
+    ></nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.kyokulink {
+  width: 40%;
+  height: 240px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 12px;
+  overflow: hidden;
+  filter: drop-shadow(4px 4px 2px rgb(0 0 0 / 0.4));
+  transition-duration: 0.4s;
+  transition-timing-function: ease-out;
+}
+.kyokulink:hover {
+  filter: drop-shadow(8px 8px 4px rgb(0 0 0 / 0.4));
+}
+.bgType1 {
+  background-image: url('~assets/images/kyoku_link/sample.jpg');
+}
+
+.link {
+  width: 100%;
+  height: 100%;
+  color: white;
+  text-decoration: none;
+}
+
+.img_filter {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (max-width: 750px) {
+  .kyokulink {
+    width: 100%;
+    height: 120px;
+  }
+}
+</style>
--- frontend/pages/ComponentsList/index.vue
+++ frontend/pages/ComponentsList/index.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    <div>
-      <h1>ComponentsList</h1>
-      <p>このページではコンポーネントをすべて表示します</p>
-    </div>
+    <h1>ComponentsList</h1>
+    <p>このページではコンポーネントをすべて表示します</p>
+    <section>
+      <h1>局紹介リンク</h1>
+      <div class="kyoku_links">
+        <OrganismsKyokuLink :bg-type="'bgType1'">会場整備局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">開発局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">企画局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">広報制作局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">財務局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">参加対応局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">渉外局</OrganismsKyokuLink>
+        <OrganismsKyokuLink :bg-type="'bgType1'">総務局</OrganismsKyokuLink>
+      </div>
+    </section>
     <section>
       <h1>Slide</h1>
       <OrganismsSlide />
     </section>
   </div>
 </template>
+
+<style scoped>
+section {
+  padding: 40px 0;
+}
+
+.kyoku_links {
+  max-width: 1200px;
+  margin: auto;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+</style>
