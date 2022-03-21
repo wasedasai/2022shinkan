181ab851c5bd6fff1cac1dc18ed3b62c0909a888
akino <akikoba2001@gmail.com>
2022-03-21 13:21:03 +0900

    局紹介リンク修正

--- frontend/components/organisms/KyokuLink/index.vue
+++ frontend/components/organisms/KyokuLink/index.vue
@@ -11,27 +11,41 @@ export default {
 </script>
 
 <template>
-  <div class="kyokulink" :class="bgType">
-    <nuxt-link :to="url" class="link"
-      ><div class="img_filter">
-        <h2><slot /></h2></div
-    ></nuxt-link>
+  <div class="kyokulink-wrapper">
+    <div class="kyokulink" :class="bgType">
+      <nuxt-link :to="url" class="link"
+        ><div class="img_filter">
+          <h2><slot /></h2></div
+      ></nuxt-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.kyokulink {
+.kyokulink-wrapper {
   width: 40%;
   height: 240px;
-  background-repeat: no-repeat;
-  background-size: cover;
+  overflow: hidden;
   border-radius: 12px;
   filter: drop-shadow(4px 4px 2px rgb(0 0 0 / 0.4));
   transition-duration: 0.4s;
   transition-timing-function: ease-out;
 }
-.kyokulink:hover {
+.kyokulink-wrapper:hover {
   filter: drop-shadow(8px 8px 4px rgb(0 0 0 / 0.4));
+}
+
+.kyokulink {
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 12px;
+  transition-duration: 0.4s;
+  transition-timing-function: ease-out;
+}
+.kyokulink:hover {
+  transform: scale(1.2);
 }
 .bgType1 {
   background-image: url('~assets/images/kyoku_link/sample.jpg');
@@ -55,7 +69,7 @@ export default {
 }
 
 @media screen and (max-width: 750px) {
-  .kyokulink {
+  .kyokulink-wrapper {
     width: 100%;
     height: 120px;
   }
