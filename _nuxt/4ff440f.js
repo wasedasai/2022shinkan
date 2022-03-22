b91b10788dbbb50defdd3c2240747261b47eedfb
airknfj <airknfj@users.noreply.github.com>
2022-03-22 14:21:18 +0000

    deploy: b7a633a332bedb1a8151c54239d863d88fcc70d4

---
+++ _nuxt/4ff440f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{316:function(e,n,t){"use strict";t.r(n);var l=t(33),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("p",[e._t("default")],2)}),[],!1,null,null,null);n.default=component.exports}}]);
