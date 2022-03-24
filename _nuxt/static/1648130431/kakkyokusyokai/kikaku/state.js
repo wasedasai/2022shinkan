12334be2663413fb7124accad7a663440a97d0ab
airknfj <airknfj@users.noreply.github.com>
2022-03-24 14:24:43 +0000

    deploy: 463c5df75ffdaeb9b3995243cfd910b76b17eaa2

--- _nuxt/static/1648130431/kakkyokusyokai/kikaku/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F2022shinkan\u002F_nuxt\u002Fstatic\u002F1648130431",layout:"default",error:a,serverRendered:true,routePath:"\u002Fkakkyokusyokai\u002Fkikaku",config:{_app:{basePath:"\u002F2022shinkan\u002F",assetsPath:"\u002F2022shinkan\u002F_nuxt\u002F",cdnURL:a}}}}(null));
