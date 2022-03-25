b0e6c71adee40fbfc141421a8f07277dae111737
aino <aino.takahashi@wasedasai.net>
2022-03-25 12:19:39 +0900

    グラフ挿入しました。二列にできてないです

--- frontend/pages/zentaizowotukamou/index.vue
+++ frontend/pages/zentaizowotukamou/index.vue
@@ -8,6 +8,64 @@
       <div class="section-inner">
         <img src="~assets/images/zentaizo_page/soshikizu.jpg" alt="組織図">
       </div>
+      <div>
+        <ul class="container">
+          <li>01.性別
+            <img src="~assets/images/zentaizo_page/seibetu.jpg" class="image1" alt="性別">
+          </li>
+          <li>02.学部
+            <img src="~assets/images/zentaizo_page/gakubu.jpg" class="image1" alt="学部">
+          </li>
+          <li>03.第二外国語
+            <img src="~assets/images/zentaizo_page/nigai.jpg" class="image1" alt="二外">
+          </li>
+          <li>04.GPA
+            <img src="assets/images/zentaizo_page/gpa.jpg" class="image1" alt="gpa">
+          </li>
+          <li>05.受験方式
+            <img src="~assets/images/zentaizo_page/jyukenhoho.jpg" class="image1" alt="受験方法">
+          </li>
+          <li>06.出身地
+            <img src="~assets/images/zentaizo_page/syussin.jpg" class="image1" alt="出身地">
+          </li>
+           <li>07.居住形態
+            <img src="~assets/images/zentaizo_page/kyojyukeitai.jpg" class="image1" alt="居住形態">
+          </li>
+          <li>08.バイトをしていますか？
+            <img src="~assets/images/zentaizo_page/baito.jpg" class="image1" alt="バイトの有無">
+          </li>
+          <li>09.バイトの種類
+            <img src="~assets/images/zentaizo_page/baito_type.jpg" class="image1" alt="バイトの種類">
+          </li>
+          <li>10.バイトの月収
+            <img src="~assets/images/zentaizo_page/baito_gessyu.jpg" class="image1" alt="バイトの月収">
+          </li>
+           <li>11.兼サ―していますか？
+            <img src="~assets/images/zentaizo_page/kensa.jpg" class="image1" alt="兼サー">
+          </li>
+          <li>12.兼サーの種類
+            <img src="~assets/images/zentaizo_page/kensa_type.jpg" class="image1" alt="兼サ―種類">
+          </li>
+          <li>13.タスクは忙しいですか？
+            <img src="~assets/images/zentaizo_page/tasuku.jpg" class="image1" alt="タスク">
+          </li>
+          <li>14.運スタに入った理由
+            <img src="~assets/images/zentaizo_page/reason.jpg" class="image1" alt="入会理由">
+          </li>
+           <li>15.運スタに満足してますか？
+            <img src="~assets/images/zentaizo_page/manzokudo.jpg" class="image1" alt="満足度">
+          </li>
+          <li>16.何年生の時に運スタに入りましたか？
+            <img src="~assets/images/zentaizo_page/nyukaijiki.jpg" class="image1" alt="入会時期">
+          </li>
+          <li>17.22であなたは何年目？
+            <img src="~assets/images/zentaizo_page/nannenme.jpg" class="image1" alt="何年目">
+          </li>
+          <li>18.恋人はいますか？
+            <img src="~assets/images/zentaizo_page/renai.jpg" class="image1" alt="恋人の有無">
+          </li>
+        </ul>
+      </div>
     </section>
   </main>
 </template>
@@ -22,5 +80,20 @@
   max-width: 1024px;
   padding: 80px 1rem;
   margin: auto;
+}
+
+.conteiner{
+  list-style: none;
+  display: flex;
+  justify-content:space-around;
+}
+
+.conteiner li{
+  color:#4c2219;
+}
+
+.image1{
+  width: 40%;
+  object-fit: cover;
 }
 </style>
