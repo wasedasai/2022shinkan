46a9c3f03646ed35d0ad7278c261251f60cb1709
Akinori13 <Akinori13@users.noreply.github.com>
2022-03-25 12:24:24 +0000

    deploy: 841d592c340099750f6180e54bf280ad374d6710

---
+++ _nuxt/static/1648211057/staffinterview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F2022shinkan\u002F_nuxt\u002Fstatic\u002F1648211057",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstaffinterview",config:{_app:{basePath:"\u002F2022shinkan\u002F",assetsPath:"\u002F2022shinkan\u002F_nuxt\u002F",cdnURL:a}}}}(null));
